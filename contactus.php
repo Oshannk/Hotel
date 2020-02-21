651913c5ad313632c150c46548a199a71aa7630e
IranUdesha <49976786+IranUdesha@users.noreply.github.com>
2020-02-21 15:22:36 +0530

    separated headder and footer to two different files. and turn all files to .php type for include above headder and footer

---
+++ contactus.php
@@ -0,0 +1,143 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+       <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/mbr-additional.css" type="text/css">
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.1/examples/carousel/">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      
+    <title>Contact Us</title>
+  </head>
+  <body>
+      <!-- Navigation Bar --> 
+     <?php include ('asset/headder.html');?>
+     
+<br>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="text-center">
+                <h2 class="font1">Location & Transportation</h2>
+            </div>
+        </div>
+    </div>   
+
+    <div class="row">
+        
+        <div class=" col-sm-8">
+            <iframe class="" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3632.3324490536966!2d80.67783791858359!3d5.961235720605917!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae137b4a71021c3%3A0x4f8404f50f0c3899!2zNcKwNTcnNDQuNCJOIDgwwrA0MCczNC4yIkU!5e0!3m2!1sen!2slk!4v1582258914768!5m2!1sen!2slk" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+        </div>
+        <div class="col-sm-4">
+            Address:- <br>
+            No.26, <br>
+            BELIATHTHA, <br>
+            MATARA, <br>
+            81100, <br>
+            SRI LANKA<br>
+Phone: <a href="tel:+94414600100">+9441 4600100 </a><br>
+Fax: <a href="tel:+94414600101">+9441 4600101</a><br>
+Sales:<a href="tel:+94414600102">+9441 4600102 ext= 8804</a> <br>
+Sales fax:+94 41 4 600101 <br>
+
+E-mail : <a href="mailto:lagoonview@gmail.com">lagoonview@gmail.com</a><br> <br>
+        </div>
+    </div>
+</div>
+
+<hr>
+
+<div class="container">
+    <div class="container-fluid">
+        <div>
+            <div class="col-sm-12">
+                <div class="text-center">
+                    <h2 class="font1">Airport Information</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-6">
+            <h3>Bandaranaike International Airport (CMB)</h3>
+            <br>
+    Airport Phone: +94 11 2 264444<br>
+    
+    Hotel direction: 106.3 miles SE<br>
+    
+    Airport shuttle service, reservation required, fee: 175 USD (one way)
+    24 Hours, 7 days a week<br>
+    Shuttle Phone: +94 414 600103<br>
+    Alternate transportation: Peppwe Life; fee: 175 USD (one way)<br>
+    Estimated taxi fare: 175 USD (one way)<br>
+    <a href="http://www.colombo-airport.com/"> CMB airport website</a> 
+
+    <div>
+       <h5 class="font2">Driving directions</h5> 
+       <p>
+        Canada Friendship Rd/AB9 to Colombo - Katunayake Expy E03. Get on Colombo-Kandy Hwy/A1 in Peliyagoda from Colombo - Katunayake Expy/E03. Get on E02 in from Biyagama Rd/B214 and Colombo - Hanwella Low Level Rd/AB10. Follow E01 to B465 in Exit from E01. Continue on B465. Drive to Matara Road/AH 43.
+        </p>
+</div>
+        </div>
+        <div class="col-sm-6">
+          <h3>  Mattala Rajapaksa International Airport (HRI)</h3>
+            Airport Phone: +94 47 2 031000 <br>
+
+            Hotel direction: 75.2 miles SW<br>
+
+            This hotel does not provide shuttle service.<br>
+
+            <a href="http://www.airport.lk/mria/#page=page-3"> HRI airport website </a>
+            <h5 class="font2">Driving directions</h5>
+            Get on Hambantota-Gonnoruwa-Meegahajandura Rd. Follow Hambantota-Gonnoruwa-Meegahajandura Rd to Exit from Hambantota-Gonnoruwa-Meegahajandura Rd. Keep right 8 min. Continue on A2. Take Beliatta-Kirinda Rd, Matara - Hakmana Road and B275 to Matara Road.
+        </div>
+    </div>
+    
+</div>
+
+<hr>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <h3 class="font1"> Other Transportation</h3>
+        </div>
+    </div>
+    <div class="row">
+         <div class="col-sm-4">
+             <h5 class="font2">Bus Staion</h5>
+             <p>Matara Bus Station
+                10.6 miles from hotel
+            </p>
+         </div>
+         <div class="col-sm-4">
+            <h5 class="font2">Bus Staion</h5>
+            <p>Matara Bus Station
+               10.6 miles from hotel
+         </div>
+         <div class="col-sm-4">
+            <h5 class="font2">Train Station</h5>
+            <p>Babarenda
+                1.5 miles from hotel
+         </div>
+    </div>
+</div>
+
+<hr>
+
+
+ <!-- Site footer --> 
+ <?php include ('asset/footer.html');?>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,204 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+       <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/mbr-additional.css" type="text/css">
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.1/examples/carousel/">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      
+    <title>Lagoon View</title>
+  </head>
+  <body >
+
+    <!-- Navigation Bar --> 
+    <?php include ('asset/headder.html');?>
+
+ <!-- Image Banner -->
+ <div >
+   <div>     
+    <main role="main">
+
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+          <li data-target="#myCarousel" data-slide-to="3"></li>
+          <li data-target="#myCarousel" data-slide-to="4"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="first-slide" src="Img/main1.jpg" alt="First slide">
+            <div class="container">
+              <div class="carousel-caption text-left">
+                <h1>Example headline.</h1>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+              </div>
+            </div>
+          </div>
+          
+          <div class="carousel-item">
+            <img class="second-slide" src="Img/main2.jpg" alt="Second slide">
+            <div class="container">
+              <div class="carousel-caption">
+                <h1>Another example headline.</h1>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img class="third-slide" src="Img/main3.jpg" alt="Third slide">
+            <div class="container">
+              <div class="carousel-caption text-right">
+                <h1>One more for good measure.</h1>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img class="fouth-slide" src="Img/main5.jpg" alt="fouth slide">
+            <div class="container">
+              <div class="carousel-caption text-right">
+                <h1>One more for good measure.</h1>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img class="fifth-slide" src="Img/main6.jpg" alt="Fifth slide">
+            <div class="container">
+              <div class="carousel-caption text-right">
+                <h1>One more for good measure.</h1>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+   </div>
+ </div>
+
+ <br>
+
+
+ <!-- Marketing messaging and featurettes
+      ================================================== -->
+      <!-- Wrap the rest of the page in another container to center all the content. -->
+
+      <div class="col-md-8" style="position: relative; left: 20%;">
+
+        <!-- Three columns of text below the carousel -->
+        <div class="row">
+          <div class="col-lg-3">
+            <img class="rounded-circle" src="Img/spa.jpg" alt="Spa" width="140" height="140">
+            <h2>Spa</h2>
+            <p>Discover peace of mind while luxuriating in our specialized Balinese Ayu Spa in Matara. Let us pamper you with a relaxing synchronic massage or simply choose from an array of revitalizing spa treatments from the menu. </p>
+            <p><br><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+          </div><!-- /.col-lg-4 -->
+          <div class="col-lg-3">
+            <img class="rounded-circle" src="Img/dining.jpg" alt="Dining" width="140" height="140">
+            <h2>Dining</h2>
+            <p>Sample the scrumptious, globally-inspired offerings at Weligama Kitchen, our signature hotel restaurant. Casual and inviting, guests can delight in the buffet-style options at breakfast, lunch or dinner.</p>
+            <p><br><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+          </div><!-- /.col-lg-4 -->
+          <div class="col-lg-3">
+            <img class="rounded-circle" src="Img/offer.jpg" alt="Offer" width="140" height="140">
+            <h2>Offers</h2>
+            <p>Looking for some fantastic hotel packages in Sri Lanka to enjoy great savings during your seaside holiday? Look no further as the deals offered here at our idyllic seaside resort will delight those who are intent on having themselves a blissful vacation.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+          </div><!-- /.col-lg-4 -->
+          <div class="col-lg-3">
+            <img class="rounded-circle" src="Img/room.jpg" alt="Room" width="140" height="140">
+            <h2>Rooms</h2>
+            <p>Experience 5-star luxury in our hotel roomsyou have approximately 50 different accommodation options. 22 of these are divided between 13 double rooms, 5 villas, 3 suites as well as the Grand Villa.</p>
+            <p><br><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+          </div><!-- /.col-lg-4 -->
+        </div><!-- /.row -->
+
+      </div>
+
+
+<hr>
+<!-- Start home-about Area -->
+<section class="container">
+  <div class="container-fluid">				
+    <div class="row justify-content-center align-items-center">
+      <div class="col-lg-6 no-padding home-about-left">
+        <img class="img-fluid" src="img/overview.jpg" alt="">
+      </div>
+      <div class="col-lg-6 no-padding home-about-right">
+        <h1>
+          OVERVIEW </h1>
+        <p>
+          <span>A TROPICAL GETAWAY</span>
+        </p>
+        <p>
+          Nestled along the untouched shores of Dickwella, a quaint town located on the southern coast of the island, 
+          Lagoon Villa Resort and Spa is among the top beach hotels in Sri Lanka. 
+          Come lounge under swaying palms, relax by the pool, enjoy long walks on the beach whilst teasing your taste buds with the catch of the day.
+        </p>
+        
+      </div>
+    </div>
+  </div>	
+</section>
+<!-- End home-about Area -->	
+      <hr>
+<!-- Start home-about Area -->
+<section class="container">
+  <div class="container">		
+
+    <div class="row justify-content-center align-items-center">
+      <div class="col-lg-6 no-padding home-about-right">
+        <h1>Visiting Place</h1>
+        <p>
+          <span>We are here to listen from you deliver exellence</span>
+        </p>
+        <p>
+          Matara is a busy town booming with commercial activities. Situated 160 kilometers from the Sri Lankan capital of Colombo, the coastal town may not feature highly on the lists of tourists. 
+        </p>
+        
+      </div>
+      <div class="col-lg-6 no-padding home-about-left">
+        <img class="img-fluid" src="img/img1.jpg" alt="">
+      </div>
+  
+    </div>
+  </div>	
+</section>
+<hr>
+<!-- End home-about Area -->	
+
+<?php include ('asset/footer.html');?>
+ 
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
+
+
+
