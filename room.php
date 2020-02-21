915e5e94dad0046b845d1fe2d901296ae8223f05
IranUdesha <49976786+IranUdesha@users.noreply.github.com>
2020-02-21 20:18:34 +0530

    add rooms

---
+++ room.php
@@ -0,0 +1,118 @@
+
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+       <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/mbr-additional.css" type="text/css">
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.1/examples/carousel/">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      
+    <title>Rooms & Suits</title>
+  </head>
+  <body>
+    
+     <!-- Navigation Bar --> 
+     <?php include ('asset/headder.html');?>
+
+    <!--Carousel Wrapper-->
+<div id="carousel-example-1z" class="carousel slide carousel-fade" data-ride="carousel">
+  <!--Indicators-->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-1z" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-1z" data-slide-to="1"></li>
+    <li data-target="#carousel-example-1z" data-slide-to="2"></li>
+  </ol>
+  <!--/.Indicators-->
+  <!--Slides-->
+  <div class="carousel-inner" role="listbox">
+    <!--First slide-->
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(130).jpg"
+        alt="First slide">
+    </div>
+    <!--/First slide-->
+    <!--Second slide-->
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(129).jpg"
+        alt="Second slide">
+    </div>
+    <!--/Second slide-->
+    <!--Third slide-->
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg"
+        alt="Third slide">
+    </div>
+    <!--/Third slide-->
+  </div>
+  <!--/.Slides-->
+  <!--Controls-->
+  <a class="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carousel-example-1z" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+  <!--/.Controls-->
+</div>
+
+
+
+<div id="main-content">
+       
+
+        <div class="site-content container">
+                  <h2 class="post-title">ROOMS
+</h2>
+            <div class="rooms-content layout-grid style-02">
+                <div class="row">
+                    <div class="room col-sm-4 clearfix">
+                        <div class="room-item">
+                            <div class="room-media">
+                                <img src="img/room.jpg" alt="room">
+                            </div>
+                            <div class="room-summary">
+                                <h3 class="room-title">
+                                    <a href="superiorroomonly.php">SUPERIOR ROOM <div style="font-size: 15px;display: inline-block;">(ROOM ONLY)</div></a>
+                                </h3>
+                                <ul class="room-info">
+                                    <li style="font-size: 13px;">• 35 SQUARE METRES</li><br>
+                                    <li style="font-size: 13px;">• SPACIOUS BATHROOM WITH GLASS ENCLOSURE</li><br>
+                                    <li style="font-size: 13px;">• 50” FLAT-SCREEN TELEVISION WITH CABLE CHANNELS </li>
+                                      <li style="font-size: 13px;">• PRIVILEGED ACCESS TO THE ICONIC INFINITY POOL AND ROOFTOP GARDEN</li>
+                                        <!--<li  style="font-size: 13px; font-weight: bold; ">• BALCONY WITH LIMITED SEA VIEW</li>-->
+                                </ul>
+                                <div class="line"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+<?php include ('asset/footer.html');?>
+<!--/.Carousel Wrapper-->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
