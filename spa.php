9de08b65dd147284a3207bcdec81cc0d66fa2e3e
IranUdesha <49976786+IranUdesha@users.noreply.github.com>
2020-02-21 23:54:13 +0530

    added spa page

---
+++ spa.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+       <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/mbr-additional.css" type="text/css">
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.1/examples/carousel/">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      
+    <title>Lagoon View</title>
+  </head>
+  <body >
+
+    <!-- Navigation Bar --> 
+    <?php include ('asset/headder.html');?>
+
+    <br>
+    <br>
+<div class="comtainer">
+    <div class="row">
+        <div class="container">
+            <img src="img/spa_banner.jpg" alt="banner">
+        </div>
+    </div>
+</div>
+    <hr>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <div>
+                <h2 class="font2">SPA TREATMENTS </h2>
+                <p>Spend a day luxuriating at the spa, where soothing massages and gentle fragrances blend into an exciting adventure for the mind, body and soul. Designed to present a serene, tranquil atmosphere to relax the senses, the spa at Mandara Spa Resort Mirissa offers a tantalizing range of treatments that are administered by the expert hands of therapists.
+               </p>
+            </div>
+            <br/>
+            <div>
+                <h2 class="font1"> Selection Of Therapeutic Experiences</h2>
+                <p>For a luxurious range of spa treatments in Negombo, look no further than our blissful haven at Jetwing Blue. The tranquil spa at our original home of Sri Lankan hospitality offers a range of therapeutic experiences from massages to facials and body scrubs, using a variety of techniques including aromatherapy as well as Shiatsu and more. A rejuvenating session with our trained therapists is bound to be the most relaxing complement to our luxury accommodation at Jetwing Blue.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+    <hr>
+<!-- End home-about Area -->	
+
+<?php include ('asset/footer.html');?>
+ 
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
