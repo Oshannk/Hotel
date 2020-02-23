0512dbd5e846bf2c61dd55509c1ceb7bd305fec6
IranUdesha <49976786+IranUdesha@users.noreply.github.com>
2020-02-23 17:59:21 +0530

    add some fade up animation to login page and add icon to home page

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="icon" type="image/png" href="images/icons/home.ico"/>
+    <link rel="icon" type="image/png" href="img/icons/home.ico">
        <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="css/main.css">
--- reservetable.php
+++ reservetable.php
@@ -99,8 +99,17 @@ $result = mysqli_query($conn,$sql);
   </div>
   
 
-<!-- 
-    <?php include ('asset/footer.html');?> -->
+                    <!-- Footer -->
+    <?php include ('asset/footer.html');?> 
+
+    <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="js/main1.js"></script>
+     <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
 	<script src="js/main1.js"></script>
 </body>
